3ce6c1c2f698fdb8ada8b2518096f9ff7139bd6b
DinoMartan <dino.martan@gmail.com>
2021-03-18 17:57:56 +0100

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Dino Martan on 18/03/2021.
+//  Copyright © 2021 Dino Martan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
